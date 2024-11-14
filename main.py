651d89ca8f9dd8d6416bb90e8e6b34a234bd6391
nicolas-puder <nicolas.puder@edu.esiee.fr>
2024-11-14 10:48:48 +0000

    un message explicatif

--- main.py
+++ main.py
@@ -1,20 +1,44 @@
+"""
+Module main pour nous exercer sur les fonctions en language python.
+"""
 from math import sqrt
 
 #### Fonction secondaire
 
 
 def isprime(p):
+    """
+    Permet de dire si le nombre p mit en paramètre est un nombre premier.
 
-    # votre code ici
+    Args : 
+        n : nombre à tester
 
-    pass
+    Return :
+        True si n premier
+        False si n non premier
+    """
+    premier = True
+    rc = int(sqrt(p))
+    if p == 1 :
+        premier = False
+    for d in range (2,rc + 1):
+        if p % d == 0:
+            premier = False
+            break
+    return premier
 
 #### Fonction principale
 
 
 def main():
+    """
+    Permet de tester la fontion isprime()
+    """
+    isprime(5)
 
-    # vos appels à la fonction secondaire ici
+    isprime(7)
+
+    isprime(30  )
 
     for n in range(100):
         if isprime(n):
